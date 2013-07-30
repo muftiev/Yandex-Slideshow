a5246829a3665e27d1a1112f6fa9c453cb539d34
muftiev.rr <muftiev.rr@yandex.ua>
2013-07-30 21:59:47 +0300

    sladeshow prototype object

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,3 +1,41 @@
 jQuery(document).ready(function(){
+
+	$("div").slideshow({
+		width: 500,
+		height: 350
+	});
 	
 });
+
+jQuery.fn.slideshow = function(options) {
+	Slideshowobject = inherit(Slideshow)
+	Slideshowobject.init(options);
+}
+
+function inherit(proto) {
+  function F() {}
+  F.prototype = proto;
+  return new F;
+}
+
+var Slideshow = {
+	width: 500,
+	height: 450,
+	autostart: true,
+	duration: 200,
+	thumb_size: 50,
+	thumb_hide: true,
+	arrow_nav: true,
+	key_nav: true,
+	loader: true,
+	username: "muftiev-rr",
+	album: "357412",
+	password: "",
+	img_count: 0,
+
+	init: function(options){
+		for (var prop in options) {
+			this[prop] = options[prop];
+		}
+	}
+}
